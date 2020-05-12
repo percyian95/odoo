fea54fe8724139b9b33cea7eb0c8af9aad0b5aab
percyian95 <65220470+percyian95@users.noreply.github.com>
2020-05-12 17:30:18 +0800

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sale Coupon Fix",
+    'author': 'Wizard Technolab',
+    "description": """Sale Coupon Customisation""",
+    "summary": """Sale Coupon Customisation""",
+    "version": "12.0.1.0.0",
+    "support": "info@wizardtechnolab.com",
+    "category": "Sale",
+    "depends": ['sale_management'],
+    "data": [],
+    'installable': True,
+}
